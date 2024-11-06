3edc99dfeb56bebfc8e0bc1938c831d765deaf35
revalina675 <5027231001@student.its.ac.id>
2024-11-06 23:24:22 +0700

    update

---
+++ src/index.ts
@@ -0,0 +1,15 @@
+import express from "express";
+
+const app = express();
+
+app.use(express.json());
+
+// check endpoint
+app.get("/", (_, response) => {
+  response.status(200).send("Server is up and running 💫");
+});
+
+const PORT = 4000;
+app.listen(PORT, () => {
+  console.log(`Express is running on Port ${PORT}`);
+});
