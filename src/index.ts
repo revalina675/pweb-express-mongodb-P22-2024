21cbb2d783a2fabe2ff9ebddc6319f7ec54d0c8d
revalina675 <5027231001@student.its.ac.id>
2024-11-07 01:26:42 +0700

    update

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,18 @@
 import express from "express";
+import connectDB from "./db-connection";
+import bookrouter from "./routes/book.route";
 
 const app = express();
 
 app.use(express.json());
-
+connectDB();   
 // check endpoint
 app.get("/", (_, response) => {
   response.status(200).send("Server is up and running 💫");
 });
 
+app.use("/books", bookrouter);
 const PORT = 4000;
 app.listen(PORT, () => {
-  console.log(`Express is running on Port ${PORT}`);
+  console.log('Express is running on Port ${PORT}');
 });
